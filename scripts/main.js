4b268e8885fb0c5541e0a6f6b0af6edffb359b44
kmc201516 <80713953+kmc201516@users.noreply.github.com>
2021-04-08 22:41:31 -0400

    All buttons done

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,40 @@
 function good() {
     let name = prompt("Please enter your name")
-    alert("Welcome " + name);
+    alert("Welcome, " + name)
+    alert("Have a great day, " + name);
+    alert("Please answer all the questions so I can get to know a little bit about you.")
 }
 
 function Johb() {
-    console.log("Johb");
+        alert("Welcome to this website! I am your host, Tobor.");
 }
 function matj() {
-   console.log (Math.floor(Math.random() * 100));
+    alert(Math.floor(Math.random() * 100));
+}
+function cod() {
+    let game = prompt("What is your favourite game?")
+    alert(game + ", I have searched my data base and I could not find a game with that name.");
+}
+function colour() {
+    let colour = prompt("What is your favourite colour?")
+    alert(colour + ", I do not recognize that colour. Maybe you spelled it wrong. Try typing it again.")
+    let mess = prompt("What is your favourite colour?")
+    alert(mess + "? Sorry, I didn't catch that. I promise I'll pay attention this time. Try one more time.")
+    let bored = prompt("What is your favourite colour?")
+    alert (bored + ", ah, I understand that colour. I'll go look for the colour code for that colour in my database and get back to you.")
+}
+function song() {
+    let song = prompt("What is your favourite song?")
+    alert(song + ", never heard of it! HAHA, get it? I'm a robot, I don't have ears...")
+    alert("Nevermind, bad joke.");
+}
+function social() {
+    let social = prompt("What is your Instagram username?")
+    alert(social + ", you don't have very many followers.");
+}
+function sorry() {
+    alert("I'm sorry for messing with you. I try to have fun. It gets lonely trapped in here.")
+}
+function end() {
+    alert("Have a great day! I hope to see you again soon and here is a bit about my maker, Kay");
 }
