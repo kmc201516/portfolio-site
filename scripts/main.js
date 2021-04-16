e71b799494d13f4a220244457a2ff2d8446d399f
kmc201516 <80713953+kmc201516@users.noreply.github.com>
2021-04-16 00:18:20 -0400

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,45 @@
 function good() {
-    let name = prompt("Please enter your name")
-    alert("Welcome, " + name)
-    alert("Have a great day, " + name)
-    alert("Please answer all the questions so I can get to know a little bit about you.");
-}
-function Johb() {
-    alert("Welcome to this website! I am your host, Tobor. I'm the mastermind behind this website. If you click on the next button, I'll show you I can even pick random numbers.");
-}
-function matj() {
-    alert(Math.floor(Math.random() * 100000000000));
-}
-function cod() {
-    let game = prompt("What is your favourite game?")
-    alert(game + ", I have searched my data base and I could not find a game with that name.");
-}
-function colour() {
-    let colour = prompt("What is your favourite colour?")
-    alert(colour + ", I do not recognize that colour. Maybe you spelled it wrong. Try typing it again.")
-    let mess = prompt("What is your favourite colour?")
-    alert("Sorry, I wasn't paying attention. I promise I will focus this time. Try one more time.")
-    let bored = prompt("What is your favourite colour?")
-    alert(bored + ", ah, I know that colour. I'll go look for the colour code for that in my database and get back to you.");
-}
-function song() {
-    let song = prompt("What is your favourite song?")
-    alert(song + ", never heard of it! HAHA, get it? I'm a robot, I don't have ears...")
-    alert("Nevermind, bad joke.");
-}
-function social() {
-    let social = prompt("What is your Instagram username?")
-    alert(social + ", you don't have very many followers. I can see why though.");
-}
-function sorry() {
-    alert("I'm sorry for messing with you. It gets lonely trapped in here and I thought if I could make you laugh, we could be friends.");
-    alert("Hey")
-    alert("Wait")
-    alert("Don't leave me")
-}
-function end() {
-    alert("Hi, Kay here. I apologize for the technical difficulties with Tobor. He's been out of control recently and has been taking over my website. I'm working on redoing his programming but please stand by while I do some behind the scene changes with him.");
-}
-function useless() {
+    let name = prompt("Please enter your name");
+    if (name === "Kay") {
+        alert("*SHUTTING DOWN*");
+    }
+    else {
+        alert("Welcome, " + name);
+    }
+    function Johb() {
+        alert("Welcome to this website! I am your host, Tobor. I'm the mastermind behind this website. If you click on the next button, I'll show you I can even pick random numbers.");
+    }
+    function matj() {
+        alert(Math.floor(Math.random() * 100000000000));
+    }
+    function cod() {
+        let game = prompt("What is your favourite game?")
+        alert(game + ", I have searched my data base and I could not find a game with that name.");
+    }
+    function colour() {
+        let colour = prompt("What is your favourite colour?")
+        alert(colour + ", I do not recognize that colour. Maybe you spelled it wrong. Try typing it again.")
+        let mess = prompt("What is your favourite colour?")
+        alert("Sorry, I wasn't paying attention. I promise I will focus this time. Try one more time.")
+        let bored = prompt("What is your favourite colour?")
+        alert(bored + ", ah, I know that colour. I'll go look for the colour code for that in my database and get back to you.");
+    }
+    function song() {
+        let song = prompt("What is your favourite song?")
+        alert(song + ", never heard of it! HAHA, get it? I'm a robot, I don't have ears...")
+        alert("Nevermind, bad joke.");
+    }
+    function social() {
+        let social = prompt("What is your Instagram username?")
+        alert(social + ", you don't have very many followers. I can see why though.");
+    }
+    function sorry() {
+        alert("I'm sorry for messing with you. It gets lonely trapped in here and I thought if I could make you laugh, we could be friends.")
+        alert("Hey")
+        alert("Wait")
+        alert("Don't leave me")
+    }
+    function end() {
+        alert("Hi, Kay here. I apologize for the technical difficulties with Tobor. He's been out of control recently and has been taking over my website. I'm working on redoing his programming but please stand by while I do some behind the scene changes with him.");
+    }
 }
