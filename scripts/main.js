4feb82dfa5056ed10753ec6a02e3aaa706565130
kmc201516 <80713953+kmc201516@users.noreply.github.com>
2021-04-22 01:55:19 -0400

    so much done

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,26 @@
-function good() {
-    let name = prompt("Please enter your name");
+function button1() {
+    let name = prompt("Please enter your first name");
     if (name === "Kay") {
         alert("*SHUTTING DOWN*");
     }
+    else if
+        (name === "Sarah")
+            alert("Welcome to my website! Thank you for a great quadmester, I learned a lot about coding!");
     else {
         alert("Welcome, " + name);
     }
-    function Johb() {
+}
+    function button2() {
         alert("Welcome to this website! I am your host, Tobor. I'm the mastermind behind this website. If you click on the next button, I'll show you I can even pick random numbers.");
+}
+    function button3() {
+        alert(Math.floor(Math.random() * 100));
     }
-    function matj() {
-        alert(Math.floor(Math.random() * 100000000000));
-    }
-    function cod() {
+    function button4() {
         let game = prompt("What is your favourite game?")
         alert(game + ", I have searched my data base and I could not find a game with that name.");
     }
-    function colour() {
+    function button5() {
         let colour = prompt("What is your favourite colour?")
         alert(colour + ", I do not recognize that colour. Maybe you spelled it wrong. Try typing it again.")
         let mess = prompt("What is your favourite colour?")
@@ -24,22 +28,21 @@ function good() {
         let bored = prompt("What is your favourite colour?")
         alert(bored + ", ah, I know that colour. I'll go look for the colour code for that in my database and get back to you.");
     }
-    function song() {
+    function button6() {
         let song = prompt("What is your favourite song?")
         alert(song + ", never heard of it! HAHA, get it? I'm a robot, I don't have ears...")
         alert("Nevermind, bad joke.");
     }
-    function social() {
+    function button7() {
         let social = prompt("What is your Instagram username?")
         alert(social + ", you don't have very many followers. I can see why though.");
     }
-    function sorry() {
+    function button8() {
         alert("I'm sorry for messing with you. It gets lonely trapped in here and I thought if I could make you laugh, we could be friends.")
         alert("Hey")
         alert("Wait")
         alert("Don't leave me")
     }
-    function end() {
+    function button9() {
         alert("Hi, Kay here. I apologize for the technical difficulties with Tobor. He's been out of control recently and has been taking over my website. I'm working on redoing his programming but please stand by while I do some behind the scene changes with him.");
     }
-}
